f7c0396abdb26842bbb97d13a408d29eddddd91c
defendergr <74200281+defendergr@users.noreply.github.com>
2021-01-25 19:09:55 +0200

    Add files via upload

--- grades.js
+++ grades.js
@@ -5,3 +5,4 @@ const calc=document.querySelector("#calc");
 const r=document.querySelector("#reset");
 const averg=document.querySelector("#averg");
 const plithos=document.querySelector("#plithos");
+const errmsg=document.querySelector("#error");
