5e918e2c4dd86efef5b00855f3ef5b96c8e39dcc
defendergr <74200281+defendergr@users.noreply.github.com>
2021-01-25 19:48:56 +0200

    Add files via upload

--- grades.js
+++ grades.js
@@ -13,11 +13,9 @@ r.addEventListener("click", epanafora);
 function calculate(){
 	var newv=parseInt(v.value);
 	v.value="";
-	if (newv<0 || newv>10){
-		errmsg.innerHTML="Ο αριθμός πρέπει να είναι από 0 έως 10";
-	}
-	else{errmsg.innerHTML="";
-	}
+	if (newv<0 || newv>10){errmsg.innerHTML="Ο αριθμός πρέπει να είναι από 0 έως 10";}
+	else if(isNaN(newv)){errmsg.innerHTML="Πρέπει να δώσεις αριθμό";}
+	else{errmsg.innerHTML="";}
 }
 
 function eleghos(){
